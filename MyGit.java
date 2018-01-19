bfdc466ef92af521b19cd2ac3442d2e8532dc045
jxchen <jxchen31@gmail.com>
2018-01-19 21:35:26 +1030

    my first try

---
+++ MyGit.java
@@ -0,0 +1,9 @@
+class MyGit {
+    public static void myGit() {
+        System.out.println("your_name");
+        System.out.println("I love Cloud Computing");
+    }
+    public static void main(String[] args) {
+        myGit();
+    }
+}
